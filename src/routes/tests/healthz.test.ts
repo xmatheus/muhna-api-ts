7ec5cb868e9d7b8564ea31aac98f887aac025062
xmatheus <matheuscorreia559@gmail.com>
2022-10-16 17:02:58 -0300

    test: check healthz

---
+++ src/routes/tests/healthz.test.ts
@@ -0,0 +1,15 @@
+import chai from "chai";
+import app from "../../app";
+import chaiHttp from "chai-http"
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+
+describe("GET /_heathz", () => {
+    it("should return status 200", async () => {
+        const response = await chai.request(app).get("/api/_healthz");
+        const expectedStatus = 200;
+        expect(response).to.have.status(expectedStatus);
+    });
+});
