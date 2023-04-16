e9ae211ba3e772292632d42bd83f2018601bb40c
xmatheus <matheuscorreia559@gmail.com>
2023-04-16 15:04:38 -0300

    test: fix ok route

--- src/routes/tests/healthz.test.ts
+++ src/routes/tests/healthz.test.ts
@@ -6,9 +6,9 @@ chai.use(chaiHttp);
 
 const expect = chai.expect;
 
-describe("GET /_heathz", () => {
+describe("GET /_ok", () => {
     it("should return status 200", async () => {
-        const response = await chai.request(app).get("/api/_healthz");
+        const response = await chai.request(app).get("/api/_ok");
         const expectedStatus = 200;
         expect(response).to.have.status(expectedStatus);
     });
